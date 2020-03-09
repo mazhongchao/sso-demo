ecb092a4e304050859d7d348fa785f21629c1581
mazhongchao <mazhongchao@gmail.com>
2020-03-09 18:11:44 +0800

    :art: improving structure

--- a-app-domain/a-page.php
+++ a-app-domain/a-page.php
@@ -1,8 +1,10 @@
 <?php
-$is_login = false;
 $url = urlencode('http://www.a.com/a-page.php');
 
+/* CAS: ST(Service Ticket) */
 $ticket = isset($_GET['ticket']) ? $_GET['ticket'] : '';
+
+/* CAS: Verifying ST at sso-domain(CAS Server). Actually, HTTPS should be used when validating ST at server. */
 if($ticket){
     $verify_url = 'http://www.sso.com/verify.php';
     $ch = curl_init($verify_url);
@@ -23,13 +25,13 @@ if($ticket){
     curl_close($ch);
 
     $ret = json_decode($response, true);
-    if ( isset($ret['ticket_verify']) && $ret['ticket_verify'] == true) {
-        //设置局部会话
-        session_id($ret['app_sessid']);
+    if (isset($ret['ticket_verify']) && $ret['ticket_verify'] == true) {
+        //设置a-app-domain的局部会话
+        session_id();
         session_start();
-        $_SESSION['ss_ticket'] = $ticket;
-        $_SESSION['user_id'] = $ret['user_id'];
-        $is_login = true;
+        $_SESSION['user_name'] = $ret['user'];
+        header('Location: http://www.a.com/a-page.php');
+        exit();
     }
     else {
         header('Location: http://www.sso.com/index.php?url='.$url);
@@ -37,14 +39,11 @@ if($ticket){
     }
 }
 else {
-    $app_sessid = $_COOKIE[session_name()] ?? '';
-    if ($app_sessid) {
-        session_id($app_sessid);
+    $session_id = $_COOKIE[session_name()] ?? '';
+    if ($session_id) {
+        session_id($session_id);
         session_start();
-        if ($_SESSION['sso_ticket']) {
-            $is_login = true;
-        }
-        else {
+        if (!$_SESSION['user_name']) {
             session_destroy();
             $_SESSION = [];
             setcookie(session_name(), '', -1, '/');
@@ -57,9 +56,8 @@ else {
         exit();
     }
 }
-if ($is_login) {
-    echo 'login a.com success!<br/>';
-    echo 'login user: '.$_SESSION['user_id'].'<br/><br/>';
-    $sso_ticket = $_SESSION['sso_ticket'];
-    echo '<a href="http://www.sso.com/logout.php?ticket='.$sso_ticket.'">退出</a>';
-}
+
+echo 'login a.com success!<br/>';
+echo 'login user: '.$_SESSION['user_name'].'<br/><br/>';
+echo '<a href="http://www.sso.com/logout.php?ticket='.$sso_ticket.'">退出</a>';
+
